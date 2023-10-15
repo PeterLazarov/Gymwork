b467bd44989aa006064ba4f257c1d0731a5ac199
Peter Lazarov <peter@Peters-MacBook-Pro.local>
2023-10-16 00:59:06 +0300

    initiol calandar nav

--- app/Workout.tsx
+++ app/Workout.tsx
@@ -1,17 +1,32 @@
 import { useAtom } from 'jotai'
 import { DateTime } from 'luxon'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 import { dateAtom } from '../atoms'
 
 const Workout = () => {
-  const [date] = useAtom(dateAtom)
+  const [date, setDate] = useAtom(dateAtom)
+
+  const today = DateTime.now().set({ hour: 0, minute: 0, second: 0 })
+  const todayDiff = Math.round(date.diff(today, 'days').days)
+  const label =
+    Math.abs(todayDiff) < 2
+      ? date.toRelativeCalendar()
+      : date.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
 
   return (
     <View>
       <Text>Workout</Text>
-      <Text>{date.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}</Text>
+      <Text>{label}</Text>
+      <Button
+        title="-"
+        onPress={() => setDate(date.minus({ days: 1 }))}
+      />
+      <Button
+        title="+"
+        onPress={() => setDate(date.plus({ days: 1 }))}
+      />
     </View>
   )
 }
