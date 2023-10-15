dd05203cf7d82511c28d765556b4dcf76fd4c8a2
Kamen Kolev <kkolev@protonmail.com>
2023-10-15 20:53:20 +0300

    Added jotai

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      'jotai/babel/plugin-react-refresh',
+      'jotai/babel/plugin-debug-label',
+      'jotai/babel/preset',
+    ],
+  }
+}
