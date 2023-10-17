888c70bbeef4f9d7ec3d6f6e51d6c85089eaa734
Peter Lazarov <peter@Peters-MacBook-Pro.local>
2023-10-17 17:20:28 +0300

    exercise type added

---
+++ enums/ExerciseType.ts
@@ -0,0 +1,10 @@
+export default {
+  WEIGHT_DISTANCE: 'weight-distance',
+  WEIGHT_TIME: 'weight-time',
+  WEIGHT: 'weight',
+  REPS_DISTANCE: 'reps-distance',
+  REPS_TIME: 'reps-time',
+  REPS: 'reps',
+  DISTANCE: 'distance',
+  TIME: 'time',
+}
