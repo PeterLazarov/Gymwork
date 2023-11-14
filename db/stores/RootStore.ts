7614e5bb102d8d2b8e3de8cacade3e5f1ece1924
Peter Lazarov <peter@Peters-MacBook-Pro.local>
2023-11-14 10:45:53 +0200

    weight increment removed from store

--- db/stores/RootStore.ts
+++ db/stores/RootStore.ts
@@ -2,7 +2,6 @@ import { Instance, SnapshotOut, types } from 'mobx-state-tree'
 
 import { ExerciseStoreModel } from './ExerciseStore'
 import { WorkoutStoreModel } from './WorkoutStore'
-import { weightIncrement } from '../../data/workout-seed-data'
 import { Exercise } from '../models'
 
 /**
@@ -13,15 +12,16 @@ export const RootStoreModel = types
   .props({
     exerciseStore: types.optional(ExerciseStoreModel, {}),
     workoutStore: types.optional(WorkoutStoreModel, {}),
-    weightIncrement: types.optional(types.number, weightIncrement),
   })
   .views(self => ({
     get openedExercise(): Exercise | undefined {
+      console.log('1 -=-=-=-=-=-=-=-')
       return self.exerciseStore.exercises.find(
         e => e.guid === self.workoutStore.openedExerciseGuid
       )
     },
     get exercisesPerformed(): Exercise[] {
+      console.log('2 -=-=-=-=-=-=-=-')
       return Object.keys(self.workoutStore.exerciseWorkouts)
         .map(id => self.exerciseStore.exercises.find(e => e.guid === id))
         .filter(Boolean)
