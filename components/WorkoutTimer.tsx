a7ae6a3142bc4c6a7e025d9413bfe34463688b52
Peter Lazarov <peter.lazarov94@gmail.com>
2023-11-17 15:17:34 +0200

    huge style extracted

--- components/WorkoutTimer.tsx
+++ components/WorkoutTimer.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite'
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 import { useStores } from '../db/helpers/useStores'
 import { SubSectionLabel } from '../designSystem/Label'
@@ -10,25 +10,27 @@ const WorkoutTimer: React.FC = () => {
   const { timeStore } = useStores()
 
   return (
-    <View
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: colors.secondary,
-        borderBottomLeftRadius: 16,
-        borderBottomRightRadius: 16,
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        marginBottom: 4,
-        width: 120,
-        alignSelf: 'center',
-      }}
-    >
+    <View style={styles.timerPanel}>
       <SubSectionLabel>{timeStore.stopwatchValue}</SubSectionLabel>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  timerPanel: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.secondary,
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    marginBottom: 4,
+    width: 120,
+    alignSelf: 'center',
+  },
+})
 
 export default observer(WorkoutTimer)
