6043e7fa4089fa6bc735203e85ea581146c23826
Peter Lazarov <peter.lazarov94@gmail.com>
2023-11-24 14:13:22 +0200

    conditions fixed

--- components/Exercise/FavoriteExerciseSelect.tsx
+++ components/Exercise/FavoriteExerciseSelect.tsx
@@ -17,13 +17,13 @@ const FavoriteExerciseSelect: React.FC<Props> = ({ onSelect }) => {
   // TODO: convert most used to favorited exercises?
   return (
     <>
-      {workoutStore.mostUsedExercises.length < 0 && (
+      {workoutStore.mostUsedExercises.length > 0 && (
         <ExerciseList
           exercises={workoutStore.mostUsedExercises}
           onSelect={onSelect ?? noop}
         />
       )}
-      {workoutStore.mostUsedExercises.length > 0 && (
+      {workoutStore.mostUsedExercises.length === 0 && (
         <View
           style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
         >
