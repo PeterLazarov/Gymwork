20280e2fd9c5ff8ac3449327baca74d88820ef14
Peter Lazarov <peter.lazarov94@gmail.com>
2024-08-14 12:42:34 +0300

    Revert "yeet"
    This reverts commit 5289b447099a9fe0823fb3a6bedfd0b9dd046d3f.

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,12 @@
 import { ExpoConfig, ConfigContext } from "@expo/config"
 
 /**
+ * Use ts-node here so we can use TypeScript for our Config Plugins
+ * and not have to compile them to JavaScript
+ */
+require("ts-node/register")
+
+/**
  * @param config ExpoConfig coming from the static config app.json if it exists
  * 
  * You can read more about Expo's Configuration Resolution Rules here:
@@ -13,6 +19,7 @@ module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => {
     ...config,
     plugins: [
       ...existingPlugins,
+      require("./plugins/withSplashScreen").withSplashScreen,
     ],
   }
 }
