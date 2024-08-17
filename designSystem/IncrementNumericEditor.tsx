206099b00cd4a64861383e0dd8f9bc62d1a58b4b
Kamen Kolev <kkolev@protonmail.com>
2024-08-17 17:11:09 +0300

    Utilized Enter key on android keyboard for adding sets or tabbing

--- designSystem/IncrementNumericEditor.tsx
+++ designSystem/IncrementNumericEditor.tsx
@@ -2,61 +2,70 @@ import React, { useEffect, useState } from 'react'
 import { TextInput } from 'react-native'
 
 import IncrementDecrementButtons from '../app/components/IncrementDecrementButtons'
+import { TextInputProps } from 'react-native-paper'
+import { forwardRef } from 'react'
 
 type Props = {
   value: number
   onChange: (value: number) => void
+  onSubmit?: TextInputProps['onSubmitEditing']
   step?: number
+  returnKeyType?: TextInputProps['returnKeyType']
 }
 
 const maxDigits = 4
 const maxDecimals = 2
 const maxLength = maxDigits + maxDecimals + 1
 
-const IncrementNumericEditor: React.FC<Props> = ({ value, onChange, step }) => {
-  const [rendered, setRendered] = useState(String(value) ?? '')
+const IncrementNumericEditor = forwardRef<TextInput, Props>(
+  ({ value, onChange, step, onSubmit, returnKeyType }, ref) => {
+    const [rendered, setRendered] = useState(String(value) ?? '')
 
-  useEffect(() => {
-    if (Number(rendered) !== value) {
-      setRendered(
-        String(value.toFixed(String(value).includes('.') ? maxDecimals : 0))
-      )
-    }
-  })
+    useEffect(() => {
+      if (Number(rendered) !== value) {
+        setRendered(
+          String(value.toFixed(String(value).includes('.') ? maxDecimals : 0))
+        )
+      }
+    })
 
-  return (
-    <IncrementDecrementButtons
-      value={value}
-      onChange={n => onChange(Math.max(n, 0))}
-      step={step}
-    >
-      <TextInput
-        style={{ flexGrow: 1, textAlign: 'center', fontSize: 16 }}
-        multiline={false}
-        keyboardType="decimal-pad"
-        onChangeText={text => {
-          const asNum = Number(text)
-          if (isNaN(asNum)) {
-            return
-          }
+    return (
+      <IncrementDecrementButtons
+        value={value}
+        onChange={n => onChange(Math.max(n, 0))}
+        step={step}
+      >
+        <TextInput
+          style={{ flexGrow: 1, textAlign: 'center', fontSize: 16 }}
+          multiline={false}
+          keyboardType="decimal-pad"
+          onSubmitEditing={onSubmit}
+          ref={ref}
+          onChangeText={text => {
+            const asNum = Number(text)
+            if (isNaN(asNum)) {
+              return
+            }
 
-          // Strip leading zeroes, enforce max length
-          const toFixed = text
-            .replace(/^0+/, '')
-            .slice(
-              0,
-              text.includes('.')
-                ? Math.min(text.indexOf('.') + maxDecimals + 1, maxLength)
-                : maxDigits
-            )
-          setRendered(toFixed)
+            // Strip leading zeroes, enforce max length
+            const toFixed = text
+              .replace(/^0+/, '')
+              .slice(
+                0,
+                text.includes('.')
+                  ? Math.min(text.indexOf('.') + maxDecimals + 1, maxLength)
+                  : maxDigits
+              )
+            setRendered(toFixed)
 
-          onChange(Number(toFixed))
-        }}
-        value={rendered}
-      />
-    </IncrementDecrementButtons>
-  )
-}
+            onChange(Number(toFixed))
+          }}
+          value={rendered}
+          returnKeyType={returnKeyType}
+        />
+      </IncrementDecrementButtons>
+    )
+  }
+)
 
 export default IncrementNumericEditor
