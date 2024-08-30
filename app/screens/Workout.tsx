7066c5dd9995109cf72769678f7ccd49e9afa079
Peter Lazarov <peter.lazarov94@gmail.com>
2024-08-30 12:41:15 +0300

    on change focus workout

--- app/screens/Workout.tsx
+++ app/screens/Workout.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useRef } from 'react'
 import { View } from 'react-native'
-import { ListRenderItemInfo } from '@shopify/flash-list'
+import { FlashList, ListRenderItemInfo } from '@shopify/flash-list'
 
 import { EmptyLayout } from 'app/layouts/EmptyLayouts'
 import DayControl from 'app/components/Workout/DayControl'
@@ -10,6 +10,7 @@ import WorkoutHeader from 'app/components/Workout/WorkoutHeader'
 import WorkoutExerciseStatsView from 'app/components/ExerciseStats/ExerciseStatsView'
 import WorkoutDayView from 'app/components/Workout/WorkoutDayView'
 import { HorizontalScreenList } from 'designSystem'
+import { useStores } from 'app/db/helpers/useStores'
 
 type Screen = {
   name: string
@@ -17,6 +18,13 @@ type Screen = {
 }
 
 const WorkoutPage: React.FC = () => {
+  const { stateStore } = useStores()
+  const flashList = useRef<FlashList<Screen>>(null)
+
+  useEffect(() => {
+    flashList.current?.scrollToIndex({ index: 1 })
+  }, [stateStore.openedDate])
+
   const screens: Screen[] = [
     { name: 'Stats', component: WorkoutExerciseStatsView },
     { name: 'Workout', component: WorkoutDayView },
@@ -34,6 +42,7 @@ const WorkoutPage: React.FC = () => {
       <DayControl />
       <View style={{ flex: 1 }}>
         <HorizontalScreenList
+          ref={flashList}
           data={screens}
           renderItem={renderItem}
           initialScrollIndex={1}
