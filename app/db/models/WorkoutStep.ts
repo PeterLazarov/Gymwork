9a63329eabda5f390e6a176937759e037a2e783f
Peter Lazarov <peter.lazarov94@gmail.com>
2024-08-30 16:36:08 +0300

    step type added

--- app/db/models/WorkoutStep.ts
+++ app/db/models/WorkoutStep.ts
@@ -4,15 +4,29 @@ import { v4 as uuidv4 } from 'uuid'
 
 import { withSetPropAction } from '../helpers/withSetPropAction'
 import { WorkoutSetModel } from './WorkoutSet'
-import { ExerciseModel } from './Exercise'
+import { Exercise } from './Exercise'
+import { uniqueValues } from 'app/utils/array'
+
+export const WorkoutStepType = {
+  SingleSet: 'singleset',
+  SuperSet: 'superset',
+}
 
 export const WorkoutStepModel = types
   .model('WorkoutStep')
   .props({
     guid: types.optional(types.identifier, () => uuidv4()),
     sets: types.array(WorkoutSetModel),
-    exercise: types.reference(ExerciseModel)
+    type: WorkoutStepType.SingleSet
   })
+  .views(step => ({
+    get exercises(): Exercise[] {
+      return uniqueValues(step.sets.map(s => s.exercise))
+    },
+    get exercise(): Exercise | null {
+      return step.type === WorkoutStepType.SingleSet ? this.exercises[0] : null
+    }
+  }))
   .actions(withSetPropAction)
 
 export interface WorkoutStep extends Instance<typeof WorkoutStepModel> {}
