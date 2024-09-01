ac5ee47e95934a031122d6defc6ac577a9488fcf
Peter Lazarov <peter.lazarov94@gmail.com>
2024-09-02 01:24:56 +0300

    more colors schemas

--- designSystem/tokens/colors.tsx
+++ designSystem/tokens/colors.tsx
@@ -1,11 +1,15 @@
 const colorSchemas = {
   purple: { default: '#764abc', light: '#AB9FCF', lighter: '#E7E0EC' },
-  tomatoRed: { default: '#FF6347', light: '#FF9987', lighter: '#FFD4C4' },
+  coral: { default: '#FF6F61', light: '#FF9D8D', lighter: '#FFCCC7' },
   green: { default: '#4CAF50', light: '#8BC68A', lighter: '#C8E6CB' },
+  darkGreen: { default: '#006400', light: '#228B22', lighter: '#66CDAA' },
   gold: { default: '#FFA726', light: '#FFB95A', lighter: '#FFE0B2' },
   indigo: { default: '#3F51B5', light: '#7986CB', lighter: '#C5CAE9' },
   blue: { default: '#2196F3', light: '#64B5F6', lighter: '#BBDEFB' },
   lightBlue: { default: '#03A9F4', light: '#4FC3F7', lighter: '#B3E5FC' },
+  teal: { default: '#009688', light: '#4DB6AC', lighter: '#B2DFDB' },
+  pink: { default: '#E91E63', light: '#F06292', lighter: '#F8BBD0' },
+  amber: { default: '#FFC107', light: '#FFD54F', lighter: '#FFECB3' },
 }
 
 export default {
@@ -13,9 +17,9 @@ export default {
   primaryLight: colorSchemas.purple.light,
   primaryLighter: colorSchemas.purple.lighter,
 
-  secondary: colorSchemas.tomatoRed.default,
-  secondaryLight: colorSchemas.tomatoRed.light,
-  secondaryLighter: colorSchemas.tomatoRed.lighter,
+  secondary: colorSchemas.coral.default,
+  secondaryLight: colorSchemas.coral.light,
+  secondaryLighter: colorSchemas.coral.lighter,
 
   neutralDark: '#626262',
   neutral: '#F7F3F9',
