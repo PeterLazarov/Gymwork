56aa8a7246796125c3cfc20169c229a1dcf99534
Peter Lazarov <peter.lazarov94@gmail.com>
2024-09-02 01:15:48 +0300

    color schema added

--- designSystem/tokens/colors.tsx
+++ designSystem/tokens/colors.tsx
@@ -1,10 +1,22 @@
+const colorSchemas = {
+  purple: { default: '#764abc', light: '#AB9FCF', lighter: '#E7E0EC' },
+  tomatoRed: { default: '#FF6347', light: '#FF9987', lighter: '#FFD4C4' },
+  green: { default: '#4CAF50', light: '#8BC68A', lighter: '#C8E6CB' },
+  gold: { default: '#FFA726', light: '#FFB95A', lighter: '#FFE0B2' },
+  indigo: { default: '#3F51B5', light: '#7986CB', lighter: '#C5CAE9' },
+  blue: { default: '#2196F3', light: '#64B5F6', lighter: '#BBDEFB' },
+  lightBlue: { default: '#03A9F4', light: '#4FC3F7', lighter: '#B3E5FC' },
+}
+
 export default {
-  primary: '#764abc',
-  primaryLight: '#AB9FCF',
-  primaryLighter: '#E7E0EC',
-  secondary: '#FF6347',
-  secondaryLight: '#FF9987',
-  secondaryLighter: '#FFD4C4',
+  primary: colorSchemas.purple.default,
+  primaryLight: colorSchemas.purple.light,
+  primaryLighter: colorSchemas.purple.lighter,
+
+  secondary: colorSchemas.tomatoRed.default,
+  secondaryLight: colorSchemas.tomatoRed.light,
+  secondaryLighter: colorSchemas.tomatoRed.lighter,
+
   neutralDark: '#626262',
   neutral: '#F7F3F9',
   critical: '#B22222',
