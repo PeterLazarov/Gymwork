c06c22ca0ea3a601b2280e99f7312f6fcdc21f0a
Peter Lazarov <peter.lazarov94@gmail.com>
2024-09-02 03:06:01 +0300

    nuances

--- designSystem/tokens/colors.tsx
+++ designSystem/tokens/colors.tsx
@@ -2,14 +2,14 @@ export const colorSchemas = {
   purple: { default: '#764abc', light: '#AB9FCF', lighter: '#E7E0EC' },
   coral: { default: '#FF6F61', light: '#FFCCC7', lighter: '#FFE5E0' },
   green: { default: '#4CAF50', light: '#8BC68A', lighter: '#C8E6CB' },
-  darkGreen: { default: '#006400', light: '#228B22', lighter: '#66CDAA' },
+  darkGreen: { default: '#006400', light: '#A2D8CE', lighter: '#E0F4F2' },
   gold: { default: '#FFA726', light: '#FFB95A', lighter: '#FFE0B2' },
-  blue: { default: '#2196F3', light: '#64B5F6', lighter: '#BBDEFB' },
-  teal: { default: '#008080', light: '#4DB6AC', lighter: '#B2DFDB' },
-  pink: { default: '#E91E63', light: '#F06292', lighter: '#F8BBD0' },
-  amber: { default: '#FFC107', light: '#FFD54F', lighter: '#FFECB3' },
-  coolMintGreen: { default: '#4DB6AC', light: '#80CBC4', lighter: '#D0F0EB' },
-  coolSlateBlue: { default: '#6A5ACD', light: '#8A6BCC', lighter: '#C5CAE9' },
+  blue: { default: '#2196F3', light: '#BBDEFB', lighter: '#D6E8FD' },
+  teal: { default: '#008080', light: '#B2DFDB', lighter: '#D0F4F2' },
+  pink: { default: '#E91E63', light: '#F8BBD0', lighter: '#FBE4E8' },
+  amber: { default: '#FFC107', light: '#FFECB3', lighter: '#FFF6E1' },
+  coolMintGreen: { default: '#4DB6AC', light: '#D0F0EB', lighter: '#E6F8F6' },
+  coolSlateBlue: { default: '#6A5ACD', light: '#C5CAE9', lighter: '#E3E8F3' },
 }
 
 export const colors = {
@@ -17,9 +17,9 @@ export const colors = {
   primaryLight: colorSchemas.purple.light,
   primaryLighter: colorSchemas.purple.lighter,
 
-  secondary: colorSchemas.coral.default,
-  secondaryLight: colorSchemas.coral.light,
-  secondaryLighter: colorSchemas.coral.lighter,
+  secondary: colorSchemas.darkGreen.default,
+  secondaryLight: colorSchemas.darkGreen.light,
+  secondaryLighter: colorSchemas.darkGreen.lighter,
 
   neutralDark: '#626262',
   neutral: '#F7F3F9',
