175f0b25b753697c2bf909f02158979e1d5e29ab
Peter Lazarov <peter.lazarov94@gmail.com>
2024-09-06 19:24:40 +0300

    pressable slider component

---
+++ designSystem/Slider.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useRef } from 'react'
+import { View, PanResponder, LayoutChangeEvent } from 'react-native'
+import MultiSlider, {
+  MultiSliderProps,
+} from '@ptomasroos/react-native-multi-slider'
+import { colors } from './tokens'
+
+type Props = Omit<MultiSliderProps, 'min' | 'max' | 'onValuesChange'> & {
+  max: number
+  min: number
+  onValuesChange: NonNullable<MultiSliderProps['onValuesChange']>
+}
+
+const CustomSlider: React.FC<Props> = ({
+  min,
+  max,
+  onValuesChange,
+  selectedStyle,
+  markerStyle,
+  ...otherProps
+}) => {
+  const sliderRef = useRef(null)
+  const [sliderWidth, setSliderWidth] = useState(0)
+
+  // Calculate value based on the touch position
+  const calculateValueFromTouch = (x: number) => {
+    const newValue = Math.round((x / sliderWidth) * (max - min) + min)
+    onValuesChange([newValue])
+  }
+
+  // Capture the layout of the slider
+  const handleLayout = (event: LayoutChangeEvent) => {
+    const { width } = event.nativeEvent.layout
+    setSliderWidth(width)
+  }
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: (event, gestureState) => {
+      const { locationX } = event.nativeEvent
+      calculateValueFromTouch(locationX)
+    },
+  })
+
+  return (
+    <View
+      ref={sliderRef}
+      onLayout={handleLayout}
+      {...panResponder.panHandlers} // Attach touch handlers
+      style={{ paddingHorizontal: 10 }}
+    >
+      <MultiSlider
+        onValuesChange={onValuesChange}
+        selectedStyle={{
+          backgroundColor: colors.primary,
+          ...selectedStyle,
+        }}
+        markerStyle={{
+          backgroundColor: colors.primary,
+          ...markerStyle,
+        }}
+        {...otherProps}
+      />
+    </View>
+  )
+}
+
+export default CustomSlider
