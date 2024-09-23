46e94012952a1e447bd41406560101b99ea515d9
Peter Lazarov <peter.lazarov94@gmail.com>
2024-09-23 13:48:56 +0300

    horizontal scroll sensitivity config

--- designSystem/HorizontalScreenList.tsx
+++ designSystem/HorizontalScreenList.tsx
@@ -6,6 +6,12 @@ import {
   ViewabilityConfig,
   ViewToken,
 } from 'react-native'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
 
 type LockedProps = 'onScroll' | 'getItemLayout' | 'horizontal'
 
@@ -40,28 +46,57 @@ const HorizontalScreenList = forwardRef<FlashList<any>, Props>(
       }
     },
     [])
+    const translationX = useSharedValue(0)
 
+    const panGesture = Gesture.Pan()
+      .activeOffsetX([-25, 25]) // This ensures horizontal gestures are recognized only when dragging past the threshold
+      .failOffsetY([-10, 10]) // If the user is scrolling more vertically, we fail the horizontal gesture
+      .onUpdate(event => {
+        // Check if horizontal translation is significant (past threshold)
+        if (Math.abs(event.translationX) > 25) {
+          // Apply horizontal translation
+          translationX.value = event.translationX
+        }
+      })
+      .onEnd(() => {
+        // Reset translation when gesture ends
+        translationX.value = withSpring(0)
+      })
+
+    const animatedStyle = useAnimatedStyle(() => ({
+      transform: [{ translateX: translationX.value }],
+    }))
     return (
-      <View style={{ flex: 1 }}>
-        <FlashList
-          ref={ref}
-          showsHorizontalScrollIndicator={false}
-          viewabilityConfig={viewabilityConfig}
-          data={data}
-          onViewableItemsChanged={handleViewChange}
-          pagingEnabled
-          keyExtractor={(item, index) => String(index)}
-          estimatedItemSize={width}
-          renderItem={props => (
-            <View style={{ width, height: '100%' }}>
-              {externalRenderItem!(props)}
-            </View>
-          )}
-          horizontal
-          initialScrollIndex={initialScrollIndex}
-          {...rest}
-        />
-      </View>
+      <GestureDetector gesture={panGesture}>
+        <Animated.View
+          style={[
+            {
+              flex: 1,
+            },
+            animatedStyle,
+          ]}
+        >
+          <FlashList
+            ref={ref}
+            showsHorizontalScrollIndicator={false}
+            viewabilityConfig={viewabilityConfig}
+            data={data}
+            onViewableItemsChanged={handleViewChange}
+            pagingEnabled
+            keyExtractor={(item, index) => String(index)}
+            estimatedItemSize={width}
+            // scrollEventThrottle={32}
+            renderItem={props => (
+              <View style={{ width, height: '100%' }}>
+                {externalRenderItem!(props)}
+              </View>
+            )}
+            horizontal
+            initialScrollIndex={initialScrollIndex}
+            {...rest}
+          />
+        </Animated.View>
+      </GestureDetector>
     )
   }
 )
