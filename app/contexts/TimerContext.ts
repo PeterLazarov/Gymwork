e4e2200cf854a2b21f4ac801a3055efc50074b7b
Kamen Kolev <kkolev@protonmail.com>
2024-09-26 14:47:03 +0300

    Moved useTimer effect to Mobx

--- app/contexts/TimerContext.ts
+++ app/contexts/TimerContext.ts
@@ -1,30 +1,9 @@
-import { useEffect, useRef } from 'react'
 import { useStores } from '../db/helpers/useStores'
-import { timerKey } from 'app/db/stores/TimerStore'
+import { restTimerKey } from 'app/db/stores/TimerStore'
 
 export function useTimer() {
-  const { stateStore, timerStore } = useStores()
+  const { timerStore } = useStores()
 
-  // TODO revisit for multiple timers
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const timer = timerStore.timers.get(timerKey)!
-
-  // TODO replace this with superset support
-  const lastExercise = useRef(stateStore.focusedStepGuid)
-  useEffect(() => {
-    if (!lastExercise.current) {
-      lastExercise.current = stateStore.focusedStepGuid
-      return
-    }
-
-    if (
-      stateStore.focusedStepGuid &&
-      stateStore.focusedStepGuid !== lastExercise.current
-    ) {
-      lastExercise.current = stateStore.focusedStepGuid
-      timer.clear()
-    }
-  }, [stateStore.focusedStepGuid])
-
-  return timer
+  return timerStore.timers.get(restTimerKey)!
 }
--- app/db/stores/TimerStore.ts
+++ app/db/stores/TimerStore.ts
@@ -1,14 +1,47 @@
-import { types, Instance, SnapshotOut } from 'mobx-state-tree'
+import { types, Instance, SnapshotOut, getParent } from 'mobx-state-tree'
 import { TimerModel } from '../models/Timer'
+import { RootStore } from './RootStore'
+import { reaction } from 'mobx'
 
-export const timerKey = 'defaultTimer'
+export const restTimerKey = 'defaultTimer'
+export const durationTimerKey = 'durationTimer'
 
 // Store to handle the timer context and stateStore interaction
-export const TimerStoreModel = types.model('TimerStore', {
-  timers: types.optional(types.map(TimerModel), {
-    [timerKey]: TimerModel.create({ id: timerKey }),
-  }),
-})
+export const TimerStoreModel = types
+  .model('TimerStore', {
+    timers: types.optional(types.map(TimerModel), {
+      [restTimerKey]: TimerModel.create({ id: restTimerKey }),
+      [durationTimerKey]: TimerModel.create({ id: durationTimerKey }),
+    }),
+  })
+  // TODO replace this with superset support
+  .actions(self => {
+    const rootStore = getParent(self) as RootStore
+    const { stateStore } = rootStore
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const restTimer = self.timers.get(restTimerKey)!
+
+    let lastStep = stateStore.focusedStepGuid
+
+    return {
+      afterAttach() {
+        reaction(
+          () => stateStore.focusedStepGuid,
+          focusedStepGuid => {
+            if (!lastStep) {
+              lastStep = stateStore.focusedStepGuid
+              return
+            }
+
+            if (focusedStepGuid && focusedStepGuid !== lastStep) {
+              lastStep = focusedStepGuid
+              restTimer.clear()
+            }
+          }
+        )
+      },
+    }
+  })
 
 export interface TimerStore extends Instance<typeof TimerStoreModel> {}
 export interface TimerStoreSnapshot
