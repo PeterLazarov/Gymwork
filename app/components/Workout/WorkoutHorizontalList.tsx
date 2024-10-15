6e24bc1f94793189aa42036e46bd21a7edb56e56
Kamen Kolev <kkolev@protonmail.com>
2024-10-15 16:09:16 +0300

    Very broken

--- app/components/Workout/WorkoutHorizontalList.tsx
+++ app/components/Workout/WorkoutHorizontalList.tsx
@@ -1,55 +1,90 @@
 import { observer } from 'mobx-react-lite'
-import React, { useEffect, useMemo, useRef } from 'react'
-import { FlatList, ListRenderItemInfo, View } from 'react-native'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+import {
+  FlatList,
+  ListRenderItemInfo,
+  useWindowDimensions,
+  View,
+} from 'react-native'
 
 import { useStores } from 'app/db/helpers/useStores'
 import { getDateRange } from 'app/utils/date'
 import { HorizontalScreenList } from 'designSystem'
 import WorkoutDayView from './WorkoutDayView'
 import WorkoutBottomControls from './WorkoutBottomControls'
+import { NavigationContainer } from '@react-navigation/native'
+import { getActiveRouteName } from 'app/navigators'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { DateTime } from 'luxon'
+
+// const today = DateTime.now()
+const visibleTabs = 15
 
 const WorkoutHorizontalList = () => {
   const { stateStore } = useStores()
-  const workoutList = useRef<FlatList<string>>(null)
 
-  const dates = useMemo(() => {
+  const [currentDate, setCurrentDate] = useState(DateTime.now().toISODate())
+
+  const allDates = useMemo(() => {
     const from = stateStore.firstRenderedDate
     const to = stateStore.lastRenderedDate
 
     return getDateRange(from, to)
   }, [])
 
-  const currentIndex = useRef(dates.indexOf(stateStore.openedDate))
-  function onScreenChange(index: number) {
-    currentIndex.current = index
-    stateStore.setOpenedDate(dates[index]!)
-  }
+  const visibleDates = useMemo(() => {
+    const currentIndex = allDates.indexOf(currentDate)
+    const datePadding = (visibleTabs - 1) / 2
 
-  const renderItem = ({ item, index }: ListRenderItemInfo<string>) => {
-    return <WorkoutDayView date={dates[index]!} />
-  }
-  useEffect(() => {
-    const index = dates.indexOf(stateStore.openedDate)
+    return allDates.slice(
+      currentIndex - datePadding,
+      currentIndex + datePadding
+    )
+  }, [currentDate])
 
-    if (index < 0 || index >= dates.length) {
-      // should not happen
-      return
-    }
-
-    if (stateStore.openedDate === dates[currentIndex.current]) return
-
-    workoutList.current?.scrollToIndex({ index, animated: false })
-  }, [stateStore.openedDate])
+  const Tab = createMaterialTopTabNavigator()
+  const dimensions = useWindowDimensions()
+  const tabWidth = dimensions.width / 3
 
   return (
     <View style={{ flex: 1 }}>
-      <HorizontalScreenList
-        ref={workoutList}
-        data={dates}
-        renderItem={renderItem}
-        onScreenChange={onScreenChange}
-        initialScrollIndex={currentIndex.current}
-      />
+      <NavigationContainer
+        independent
+        // theme={navTheme}
+        onStateChange={state => {
+          if (state) {
+            const date = getActiveRouteName(state)
+            console.log({ date })
+            setCurrentDate(date)
+          }
+        }}
+      >
+        <Tab.Navigator
+          screenOptions={{
+            tabBarScrollEnabled: true,
+            tabBarItemStyle: {
+              minWidth: tabWidth,
+              width: 'auto',
+            },
+          }}
+          backBehavior="none"
+          initialRouteName={currentDate}
+        >
+          {visibleDates.map(date => (
+            <Tab.Screen
+              key={date}
+              name={date}
+            >
+              {() => {
+                return visibleDates.includes(date) ? (
+                  <WorkoutDayView date={date} />
+                ) : null
+              }}
+            </Tab.Screen>
+          ))}
+        </Tab.Navigator>
+      </NavigationContainer>
+
       <WorkoutBottomControls />
     </View>
   )
