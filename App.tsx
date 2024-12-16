0d7c4a842d3a41d5585fc7bc3f06f2819713bbc5
Kamen Kolev <kkolev@protonmail.com>
2024-12-16 21:50:14 +0200

    moved assets, configs and package.json

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import "@expo/metro-runtime"
 import * as SplashScreen from "expo-splash-screen"
 import App from "@/app"
+import '@total-typescript/ts-reset/dom'
 
 SplashScreen.preventAutoHideAsync()
 
