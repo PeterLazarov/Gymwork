821a2426cef99e3fce53555c2d089275b05c2036
PeterLazarov <peter.lazarov@dext.com>
2025-02-27 17:45:59 +0200

    test

--- app/utils/useExport.ts
+++ app/utils/useExport.ts
@@ -1,14 +1,11 @@
 import * as DocumentPicker from 'expo-document-picker'
-import {
-  StorageAccessFramework,
-  writeAsStringAsync,
-  readAsStringAsync,
-} from 'expo-file-system'
+import { StorageAccessFramework, writeAsStringAsync } from 'expo-file-system'
 import { DateTime } from 'luxon'
 import { getSnapshot } from 'mobx-state-tree'
 import { Alert } from 'react-native'
 
 import { useStores } from 'app/db/helpers/useStores'
+import data from 'app/db/seeds/pJSON_migrated.json'
 
 export function useExport() {
   const rootStore = useStores()
@@ -47,13 +44,13 @@ export function useExport() {
       })
 
       if (!result.canceled) {
-        const fileUri = result.assets[0]!.uri
+        // const fileUri = result.assets[0]!.uri
 
-        // Read the file's content
-        const fileContent = await readAsStringAsync(fileUri)
+        // // Read the file's content
+        // const fileContent = await readAsStringAsync(fileUri)
 
-        // Parse the JSON data
-        const data = JSON.parse(fileContent)
+        // // Parse the JSON data
+        // const data = JSON.parse(fileContent)
 
         rootStore.setProp('workoutStore', data?.workoutStore)
         rootStore.setProp('exerciseStore', data?.exerciseStore)
