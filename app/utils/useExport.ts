82a4a403499033e24a9a832abdfea4ba680a8159
Peter Lazarov <peter.lazarov94@gmail.com>
2025-02-28 14:43:18 +0200

    fix for migration

--- app/utils/useExport.ts
+++ app/utils/useExport.ts
@@ -1,11 +1,11 @@
 import * as DocumentPicker from 'expo-document-picker'
-import { StorageAccessFramework, writeAsStringAsync } from 'expo-file-system'
+import { StorageAccessFramework, readAsStringAsync, writeAsStringAsync } from 'expo-file-system'
 import { DateTime } from 'luxon'
 import { getSnapshot } from 'mobx-state-tree'
 import { Alert } from 'react-native'
 
 import { useStores } from 'app/db/helpers/useStores'
-import data from 'app/db/seeds/pJSON_migrated.json'
+import { RootStore } from '@/db/stores/RootStore'
 
 export function useExport() {
   const rootStore = useStores()
@@ -44,16 +44,16 @@ export function useExport() {
       })
 
       if (!result.canceled) {
-        // const fileUri = result.assets[0]!.uri
+        const fileUri = result.assets[0]!.uri
 
-        // // Read the file's content
-        // const fileContent = await readAsStringAsync(fileUri)
+        // Read the file's content
+        const fileContent = await readAsStringAsync(fileUri)
 
-        // // Parse the JSON data
-        // const data = JSON.parse(fileContent)
+        // Parse the JSON data 
+        const data = JSON.parse(fileContent) as Partial<RootStore>
 
-        rootStore.setProp('workoutStore', data?.workoutStore)
         rootStore.setProp('exerciseStore', data?.exerciseStore)
+        rootStore.setProp('workoutStore', data?.workoutStore)
         recordStore.determineRecords()
       }
     } catch (error: any) {
