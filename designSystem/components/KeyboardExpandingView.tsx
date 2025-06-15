8697c7611c4ed08b8ae89789c31ccec8a35028c1
Peter Lazarov <peter.lazarov@dext.com>
2025-06-15 09:12:18 +0300

    keyboard avoiding test

--- designSystem/components/KeyboardExpandingView.tsx
+++ designSystem/components/KeyboardExpandingView.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react'
-import { Keyboard } from 'react-native'
+import { Keyboard, Platform } from 'react-native'
 import Animated, { useSharedValue } from 'react-native-reanimated'
 
 export type KeyboardExpandingViewProps = {
   footerHeight?: number
 }
 
+const isOlderAndroidDevice = Platform.OS === 'android' && Platform.Version < 26
+
 export const KeyboardExpandingView: React.FC<KeyboardExpandingViewProps> = ({
   footerHeight = 0,
 }) => {
-  // Used in older devices where useAnimatedKeyboard doesn't work. Tested on Nexus 4 API 23 emulator
   const kbHeight = useSharedValue(0)
+
   useEffect(() => {
+    if (!isOlderAndroidDevice) return
+
     const subs = [
       Keyboard.addListener('keyboardDidShow', ({ endCoordinates }) => {
         kbHeight.value = endCoordinates.height
@@ -25,6 +29,8 @@ export const KeyboardExpandingView: React.FC<KeyboardExpandingViewProps> = ({
       subs.forEach(sub => sub.remove())
     }
   }, [])
+
+  if (!isOlderAndroidDevice) return null
 
   return <Animated.View style={{ height: kbHeight }} />
 }
