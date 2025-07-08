a069a9a9eeecbbe8dd03e6835b973df96a855f25
Kamen Kolev <kkolev@protonmail.com>
2025-07-08 17:12:56 +0300

    basic three tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,29 +4,42 @@ import MaterialIcons from "@react-native-vector-icons/material-icons"
 import { NativeTabs } from "@/components/NativeTabs"
 
 const homeIconSource = MaterialIcons.getImageSourceSync("home", 24, "black")!
+const settingsIconSource = MaterialIcons.getImageSourceSync("settings", 24, "black")!
+const reviewIconSource = MaterialIcons.getImageSourceSync("stacked-line-chart", 24, "black")!
 
 export default function TabLayout() {
   return (
-    <NativeTabs>
+    <NativeTabs
+      // tabBarActiveTintColor={"red"}
+      // activeIndicatorColor={"brown"}
+      // tabBarInactiveTintColor={"green"}
+      translucent={true}
+    >
+      <NativeTabs.Screen
+        name="review"
+        options={{
+          title: "Review",
+          tabBarIcon: () =>
+            Platform.OS === "ios" ? { sfSymbol: "chart.line.uptrend.xyaxis" } : reviewIconSource,
+        }}
+      />
+
       <NativeTabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: () =>
-            Platform.select({
-              ios: { sfSymbol: "house" },
-              android: homeIconSource,
-              // default: homeIconSource,
-            }),
+          tabBarIcon: () => (Platform.OS === "ios" ? { sfSymbol: "house" } : homeIconSource),
         }}
       />
-      {/* <NativeTabs.Screen
-        name="explore"
+
+      <NativeTabs.Screen
+        name="settings"
         options={{
-          title: "Explore",
-          tabBarIcon: () => ({ sfSymbol: "person" }),
+          title: "Settings",
+          tabBarIcon: () =>
+            Platform.OS === "ios" ? { sfSymbol: "gearshape" } : settingsIconSource,
         }}
-      /> */}
+      />
     </NativeTabs>
   )
 }
---
+++ src/app/(tabs)/settings.tsx
@@ -0,0 +1,10 @@
+import { Screen } from "@/components/Ignite/Screen"
+import { Text } from "@/components/Ignite/Text"
+
+export default function SettingsScreen() {
+  return (
+    <Screen>
+      <Text>WIP Settings Screen</Text>
+    </Screen>
+  )
+}
