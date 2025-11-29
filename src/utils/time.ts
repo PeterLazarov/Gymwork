cfb5832427bbf32ecc260dd68503fce79bc5bb36
Peter Lazarov <peter.lazarov94@gmail.com>
2025-11-29 20:09:55 +0200

    fix time formatting

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,14 +1,12 @@
 import { Duration } from 'luxon'
 
-// able to group by a nested value of a singular object or by an array of primitice values
 export const getFormatedDuration = (
-  totalSeconds: number,
+  milliseconds: number,
   hideHours?: boolean
 ) => {
-  const duration = Duration.fromObject({ seconds: totalSeconds })
+  const duration = Duration.fromObject({ milliseconds })
 
   const format = hideHours ? 'mm:ss' : 'hh:mm:ss'
-  // Format the duration to "mm:ss" format
   const formattedTime = duration.toFormat(format)
 
   return formattedTime
