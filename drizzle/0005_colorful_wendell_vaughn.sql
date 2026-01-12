ef5c671860fa5bf9280afd45cc763a0b77b4f321
Peter Lazarov <peter.lazarov94@gmail.com>
2026-01-12 18:17:32 +0200

    fix indices migration

--- drizzle/0005_colorful_wendell_vaughn.sql
+++ drizzle/0005_colorful_wendell_vaughn.sql
@@ -1,6 +1,6 @@
-CREATE INDEX `sets_workout_step_id_idx` ON `sets` (`workout_step_id`);--> statement-breakpoint
-CREATE INDEX `sets_exercise_id_idx` ON `sets` (`exercise_id`);--> statement-breakpoint
-CREATE INDEX `sets_date_idx` ON `sets` (`date`);--> statement-breakpoint
-CREATE INDEX `workout_step_exercises_workout_step_id_idx` ON `workout_step_exercises` (`workout_step_id`);--> statement-breakpoint
-CREATE INDEX `workout_steps_workout_id_idx` ON `workout_steps` (`workout_id`);--> statement-breakpoint
-CREATE INDEX `workouts_date_idx` ON `workouts` (`date`);
+CREATE INDEX IF NOT EXISTS `sets_workout_step_id_idx` ON `sets` (`workout_step_id`);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `sets_exercise_id_idx` ON `sets` (`exercise_id`);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `sets_date_idx` ON `sets` (`date`);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `workout_step_exercises_workout_step_id_idx` ON `workout_step_exercises` (`workout_step_id`);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `workout_steps_workout_id_idx` ON `workout_steps` (`workout_id`);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `workouts_date_idx` ON `workouts` (`date`);
